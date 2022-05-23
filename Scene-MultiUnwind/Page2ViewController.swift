682cb7ebe3ce03860c81e30f71fd115209063fae
michelle <lune111200@gmail.com>
2022-05-23 17:33:05 +0900

    multi unwind

---
+++ Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by SeungYeon Yoo on 2022/05/23.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+    
+    @IBAction func gotoPage2(_ segue: UIStoryboardSegue) {
+        
+    }
+    
+}
---
+++ Scene-MultiUnwind/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by SeungYeon Yoo on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func gotoPage1(_ sender: UIStoryboardSegue) {
+        
+    }
+}
+
