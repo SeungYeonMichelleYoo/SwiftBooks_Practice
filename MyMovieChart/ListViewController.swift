901d5f55ff015e530be974980d15253a9ff61278
michelle <lune111200@gmail.com>
2022-06-12 09:59:44 +0900

    moviechart final

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -7,40 +7,118 @@
 
 import UIKit
 class ListViewController: UITableViewController {
-    //튜플 아이템으로 구성된 데이터 세트
-    var dataset = [
-        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpg"),
-        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpg"),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpg")
-    ]
+    //현재까지 읽어온 데이터의 페이지 정보
+    var page = 1
     
     //테이블 뷰를 구성할 리스트 데이터
     lazy var list: [MovieVO] = {
         var datalist = [MovieVO]()
-        for (title, desc, opendate, rating, thumbnail) in self.dataset {
-            let mvo = MovieVO()
-            mvo.title = title
-            mvo.description = desc
-            mvo.opendate = opendate
-            mvo.thumbnail = thumbnail
-            mvo.rating = rating
-            
-            datalist.append(mvo)
-        }
         return datalist
     }()
     
+    @IBOutlet var moreBtn: UIButton!
     
-    override func viewDidLoad() {
+    //더보기 버튼을 눌렀을 때 호출되는 메소드
+    @IBAction func more(_ sender: Any) {
+        //현재 페이지 값에 1을 추가한다.
+        self.page += 1
+        //영화차트 API를 호출한다.
+        self.callMovieAPI()
+        //데이터를 다시 읽어오도록 테이블뷰를 갱신한다.
+        self.tableView.reloadData()
     }
+        //뷰가 처음 메모리에 로드될 때 호출되는 메소드
+        override func viewDidLoad() {
+        //영화 차트 API를 호출한다.
+        self.callMovieAPI()
+    }
+    
+    //영화 차트 API를 호출해주는 메소드
+    func callMovieAPI(){
+        
+        //1)호핀 API호출을 위한 URI를 생성
+        let url = "http://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=\(self.page)&count=30&genreId=&order=releasedateas"
+        let apiURI: URL! = URL(string: url)
+        
+        //2)REST API를 호출
+        let apidata = try! Data(contentsOf: apiURI)
+        
+        //3)데이터 전송 결과를 로그로 출력 (반드시 필요한 코드는 아님)
+        let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? ""
+        NSLog("API Result=\(log)")
+        
+        //4)JSON객체를 파싱하여 NSDictionary 객체로 변환
+        do {
+            let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options:[])
+            as! NSDictionary
+            
+        //5)데이터 구조에 따라 차례대로 캐스팅하며 읽어온다.
+        let hoppin = apiDictionary["hoppin"] as! NSDictionary
+        let movies = hoppin["movies"] as! NSDictionary
+        let movie = movies["movie"] as! NSArray
+            
+        //6) Iterator 처리를 하면서 API데이터를 MovieVO 객체에 저장.
+        for row in movie {
+            //순회 상수를 NSDictionary타입으로 캐스팅
+            let r = row as! NSDictionary
+            
+            //테이블뷰 리스트를 구성할 데이터 형식
+            let mvo = MovieVO()
+            
+            //movie 배열의 각 데이터를 mvo상수의 속성에 대입
+            mvo.title = r["title"] as? String
+            mvo.description = r["genreNames"] as? String
+            mvo.thumbnail = r["thumbnailImage"] as? String
+            mvo.detail = r["linkUrl"] as? String
+            mvo.rating = ((r["ratingAverage"] as! NSString).doubleValue)
+            
+            //웹상에 있는 이미지를 읽어와 UIImage객체로 생성
+            let url: URL! = URL(string: mvo.thumbnail!)
+            let imageData = try! Data(contentsOf: url)
+            mvo.thumbnailImage = UIImage(data: imageData)
+            
+            //list배열에 추가
+            self.list.append(mvo)
+                
+            //7)전체 데이터 카운트를 얻는다.
+            let totalCount = (hoppin["totalCount"] as? NSString)!.integerValue
+                
+            //8)totalCount가 읽어온 데이터 크기와 같거나 클 경우 더보기 버튼을 막는다.
+            if (self.list.count >= totalCount) {
+                self.moreBtn.isHidden = true
+            }
+        }
+    } catch {
+        NSLog("Parse Error!")
+    }
+}
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.list.count
     }
     
+    func getThumbnailImage(_ index: Int) -> UIImage {
+        //인자값으로 받은 인덱스를 기반으로 해당하는 배열 데이터를 읽어옴
+        let mvo = self.list[index]
+        
+        //메모이제이션: 저장된 이미지가 있으면 그것을 반환하고, 없을 경우 내려받아 저장한 후 반환
+        if let savedImage = mvo.thumbnailImage {
+            return savedImage
+        } else {
+            let url: URL! = URL(string: mvo.thumbnail!)
+            let imageData = try! Data(contentsOf: url)
+            mvo.thumbnailImage = UIImage(data:imageData) //UIImage를 MovieVO객체에 우선 저장
+            
+            return mvo.thumbnailImage! //저장된 이미지를 반환
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //주어진 행에 맞는 데이터 소스를 읽어온다.
         let row = self.list[indexPath.row]
+        
+        //로그 출력
+        NSLog("제목:\(row.title!), 호출된 행번호:\(indexPath.row)")
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! MovieCell
         
@@ -49,30 +127,32 @@ class ListViewController: UITableViewController {
         cell.desc?.text = row.description
         cell.opendate?.text = row.opendate
         cell.rating?.text = "\(row.rating!)"
-        cell.thumbnail.image = UIImage(named: row.thumbnail!)
         
-//        //테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
-//
-//        //영화제목이 표시될 레이블을 title변수로 받음
-//        let title = cell.viewWithTag(101) as? UILabel
-//        //영화 요약이 표시될 레이블을 desc변수로 받음
-//        let desc = cell.viewWithTag(102) as? UILabel
-//        //영화 개봉일이 표시될 레이블을 opendate변수로 받음
-//        let opendate = cell.viewWithTag(103) as? UILabel
-//        //영화 별점이 표시될 레이블을 rating변수로 받음
-//        let rating = cell.viewWithTag(104) as? UILabel
-//        //데이터 소스에 저장된 값을 각 레이블 변수에 할당
-//        title?.text = row.title
-//        desc?.text = row.description
-//        opendate?.text = row.opendate
-//        rating?.text = "\(row.rating!)"
+       //비동기 방식으로 섬네일 이미지를 읽어옴
+        DispatchQueue.main.async(execute: {
+            cell.thumbnail.image = self.getThumbnailImage(indexPath.row)
+        })
         
-        //구성된 셀을 반환함
+        //셀 객체를 반환
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         NSLog("선택된 행은 \(indexPath.row)번째 행입니다.")
+    }
+}
+
+// MARK: - 화면 전환 시 값을 넘겨주기 위한 세그웨이 관련 처리
+extension ListViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //실행된 세그웨이의 식별자가 "segue_detail"이라면
+        if segue.identifier == "segue_detail" {
+            //사용자가 클릭한 행을 찾아낸다.
+            let path = self.tableView.indexPath(for: sender as! MovieCell)
+            
+            //행 정보를 통해 선택된 영화 데이터를 찾은 다음, 목적지 뷰 컨트롤러의 mvo변수에 대입한다.
+            let detailVC = segue.destination as? DetailViewController
+            detailVC?.mvo = self.list[path!.row]
+        }
     }
 }
---
+++ MyMovieChart/TheaterCell.swift
@@ -0,0 +1,16 @@
+//
+//  TheaterCell.swift
+//  MyMovieChart
+//
+//  Created by SeungYeon Yoo on 2022/06/11.
+//
+
+import UIKit
+class TheaterCell: UITableViewCell {
+    
+    @IBOutlet var name: UILabel!
+    
+    @IBOutlet var tel: UILabel!
+    
+    @IBOutlet var addr: UILabel!
+}
